d0713246a85b4a2e6ed20ba188ed7c20da1ce193
unknown <cole.yadron@gmail.com>
2023-02-03 18:47:39 -0800

    restructure of part a got rid of object oriented, added sys args, prints in stdout

--- digraph.py
+++ digraph.py
@@ -1,29 +1,41 @@
 import random
+import sys
 
-class DiGraph:
-    def __init__(self, num_vertices, is_connected):
-        self.num_vertices = num_vertices
-        self.graph = {i: [] for i in range(num_vertices)}
-        self.is_connected = is_connected
+def genDigraph(num_vertices, is_connected):
+    G = {}
+    for i in range(num_vertices):
+        G[i] = []
+    if is_connected:
+        for i in range(num_vertices - 1):
+            for j in range(i+1, num_vertices):
+                G[i].append(j)
+    else:
+        for i in range(num_vertices):
+            for j in range(num_vertices):
+                if i != j and random.random() < 0.5:
+                    G[i].append(j)
+    
+    return G
 
-        if is_connected:
-            for i in range(num_vertices - 1):
-                j = i + 1
-                self.graph[i].append(j)
-                self.graph[j].append(i)
-
+def main():
+    if len(sys.argv) == 1:
+        num_vertices = random.randint(1,20)
+        connect = random.randint(1,2)
+        if connect == 1:
+            is_connected = True
         else:
-            for i in range(num_vertices):
-                for j in range(i + 1, num_vertices):
-                    if random.random() > 0.5:
-                        self.graph[i].append(j)
+            is_connected = False
+        digraph = genDigraph(num_vertices, is_connected)
+    elif len(sys.argv) < 3:
+        print("Not enough arguments")
+        return
+    else: 
+        num_vertices = int(sys.argv[1])
+        is_connected = (sys.argv[2] == "connected")
+        digraph = genDigraph(num_vertices, is_connected)
+    for i in digraph:
+     for j in digraph[i]:
+        print(f"{i} {j}")
 
-    def display(self):
-        print("Graph:")
-        for i in range(self.num_vertices):
-            print(i, "->", self.graph[i])
 
-num_vertices = int(input("Enter the number of vertices: "))
-is_connected = input("Is the graph connected (yes/no)? ") == "yes"
-graph = DiGraph(num_vertices, is_connected)
-graph.display()
+main()
