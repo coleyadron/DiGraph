8998bc88e0f1ca0c0df5f204c08e00a6a354f726
unknown <cole.yadron@gmail.com>
2023-02-10 20:32:41 -0800

    Changed the input mech a bit. Started Digraph class that will take users edges (remeber to use DFS search for DAG and connected methods)

--- pathDigraph.py
+++ pathDigraph.py
@@ -2,22 +2,28 @@
 take argument in form of edges and then store them to undertsand the graph then determine 
 the # of vertices and edges, if DAG, and the path
 '''
-edges = {}
-
+from collections import defaultdict
+edges = []
 print("Enter edges in format 'a b': (ctrl + d to stop)")
 while True:
     try:
         edge = input()
         if edge == "stop":
-            break
+             break
         v1, v2 = edge.split()
-        if (v1, v2) in edges.keys() or (v2, v1) in edges.keys():
+        if (v1, v2) in edges or (v2, v1) in edges:
             print("Duplicate edge found, renter new edge.")
         else:
-            edges[(v1, v2)] = True
+             edges.append((v1, v2))
     except EOFError:
-        break
+         break
 
-print("Edges:")
-for edge in edges:
-    print(edge)
+print(edges)
+
+class DirectedGraph:
+    def __init__(self, edges):
+        self.graph = defaultdict(list)
+        for edge in edges:
+            u, v = edge
+            self.graph[u].append(v)
+        self.visited = {}
