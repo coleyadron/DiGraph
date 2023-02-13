ca7a5b28a6b161bd1c9da4adfa6a3b112483d21e
unknown <cole.yadron@gmail.com>
2023-02-12 22:14:37 -0800

    Added the path finder from start to end. Still need sys args. Wrote DFS and added a check for connection

--- pathDigraph.py
+++ pathDigraph.py
@@ -27,3 +27,37 @@ class DirectedGraph:
             u, v = edge
             self.graph[u].append(v)
         self.visited = {}
+    
+    def find_path(self, start, end, path):
+        path.append(start)
+        if start == end:
+            return True
+        for node in self.graph[start]:
+            if node not in path:
+                if self.find_path(node, end, path):
+                    return True
+        path.pop()
+        return False
+    
+    def is_connected(self):
+        start = next(iter(self.graph.keys()))
+        self.dfs(start)
+        return len(self.visited) == len(self.graph)
+
+    def dfs(self, node):
+        self.visited[node] = True
+        for neighbor in self.graph[node]:
+            if neighbor not in self.visited:
+                self.dfs(neighbor)
+
+graph = DirectedGraph(edges)
+if graph.is_connected():
+        print("The graph is a connected.")
+else:
+        print("The graph is not a connected.")
+start, end = "a", "q3"
+path = []
+if graph.find_path(start, end, path):
+    print("Found path from", start, "to", end, ":", path)
+else:
+    print("No path found from", start, "to", end)
